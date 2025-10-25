bc5c6f29c66142542f25675f674640a9995bd3f9
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-10-25 13:12:12 -0700

    Añadir funcionalidad para eliminar tareas

--- 12_lista_tareas/src/tareas.js
+++ 12_lista_tareas/src/tareas.js
@@ -7,17 +7,25 @@ addBtn.addEventListener("click", () => {
     task.classList.add("task");
 
     const taskContent = document.createElement("span");
+    taskContent.classList.add("task-text")
     taskContent.innerText = taskInput.value;
     
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.style.marginRight = "1rem";
     checkbox.classList.add("checkbox");
     checkbox.addEventListener("change", () => {
-        task.classList.toggle("completed");
-    })
+        taskContent.classList.toggle("completed");
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.addEventListener("click", () => {
+        task.remove();
+    });
 
     task.appendChild(checkbox);
     task.appendChild(taskContent);
+    task.appendChild(deleteBtn);
     taskList.appendChild(task);
 });
