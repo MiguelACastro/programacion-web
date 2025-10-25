0c995fdc2e99b0dbfdb17f5c6627d521883e8a99
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-10-25 13:30:13 -0700

    Factorizar codigo js

--- 12_lista_tareas/src/tareas.js
+++ 12_lista_tareas/src/tareas.js
@@ -2,30 +2,33 @@ const addBtn = document.getElementById("add");
 const taskInput = document.getElementById("task");
 const taskList = document.getElementById("task-list");
 
-addBtn.addEventListener("click", () => {
+function addTask(content) {
     const task = document.createElement("li");
     task.classList.add("task");
-
+    
     const taskContent = document.createElement("span");
     taskContent.classList.add("task-text")
-    taskContent.innerText = taskInput.value;
+    taskContent.innerText = content;
     
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     checkbox.classList.add("checkbox");
     checkbox.addEventListener("change", () => {
         taskContent.classList.toggle("completed");
+        
     });
-
+    
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText = "X";
     deleteBtn.classList.add("delete-btn");
     deleteBtn.addEventListener("click", () => {
         task.remove();
     });
-
+    
     task.appendChild(checkbox);
     task.appendChild(taskContent);
     task.appendChild(deleteBtn);
     taskList.appendChild(task);
-});
+}
+
+addBtn.addEventListener("click", () => addTask(taskInput.value));
