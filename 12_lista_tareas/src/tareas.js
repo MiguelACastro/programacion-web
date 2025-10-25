31e020ba049f1b39eb9f0fbe474f5c0c2749a805
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-10-25 12:41:47 -0700

    Añadir funcionalidad para marcar tareas como completadas

--- 12_lista_tareas/src/tareas.js
+++ 12_lista_tareas/src/tareas.js
@@ -5,7 +5,19 @@ const taskList = document.getElementById("task-list");
 addBtn.addEventListener("click", () => {
     const task = document.createElement("li");
     task.classList.add("task");
-    task.innerText = taskInput.value;
-    taskList.appendChild(task);
+
+    const taskContent = document.createElement("span");
+    taskContent.innerText = taskInput.value;
     
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.style.marginRight = "1rem";
+    checkbox.classList.add("checkbox");
+    checkbox.addEventListener("change", () => {
+        task.classList.toggle("completed");
+    })
+
+    task.appendChild(checkbox);
+    task.appendChild(taskContent);
+    taskList.appendChild(task);
 });
